9c23865c24cf2d5f05ae3c147be58002ae009499
Daniel Vincenzi <danielvincenzi@icloud.com>
2020-11-28 21:23:17 -0300

    Add autoscaling settings & monolith node pool

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "google_container_cluster" "cluster" {
   location = local.zone
 
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = "1"
 
   provider = google-beta
 
@@ -50,62 +50,80 @@ resource "google_container_cluster" "cluster" {
   ]
 }
 
-#resource "google_container_node_pool" "general_node_pool" {
-#  project    = local.project
-#  name       = "${local.name}-general-node-pool"
-#  location   = local.zone
-#  cluster    = google_container_cluster.cluster.name
-#  node_count = "1"
-#
-#  node_config {
-#    oauth_scopes = [
-#      "https://www.googleapis.com/auth/logging.write",
-#      "https://www.googleapis.com/auth/monitoring",
-#    ]
-#
-#  labels = {
-#    env = local.project
-#    app = "general"
-#  }
-#
-#    preemptible  = true
-#    machine_type = "n1-standard-1"
-#    tags         = [ "kubernetes-engine-node", "${local.project}-kubernetes-engine", "general" ]
-#    metadata = {
-#      disable-legacy-endpoints = "true"
-#    }
-#  }
-#
-#  depends_on = [
-#    google_project_service.container_googleapis,
-#    google_project_service.cloudresourcemanager_googleapis
-#  ]
-#}
-
-resource "google_container_node_pool" "cart_node_pool" {
-  project    = local.project
-  name       = "${local.name}-cart-node-pool"
-  location   = local.zone
-  cluster    = google_container_cluster.cluster.name
-  node_count = "1"
+resource "google_container_node_pool" "monolith_node_pool" {
+  project  = local.project
+  name     = "${local.name}-monolith-node-pool"
+  location = local.zone
+  cluster  = google_container_cluster.cluster.name
 
   node_config {
     oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/monitoring"
     ]
 
-  labels = {
-    env = local.project
-    app = "cart"
-  }
+    labels = {
+      app = "monolith"
+    }
 
     preemptible  = true
     machine_type = "n1-standard-1"
-    tags         = [ "kubernetes-engine-node", "${local.project}-kubernetes-engine", "cart" ]
+    tags         = ["kubernetes-engine-node", "${local.project}-kubernetes-engine", "monolith"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
+  }
+
+  management {
+    auto_repair = true
+  }
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "1"
+  }
+
+  depends_on = [
+    google_project_service.cloudresourcemanager_googleapis,
+    google_project_service.container_googleapis
+  ]
+}
+
+resource "google_container_node_pool" "cart_node_pool" {
+  project  = local.project
+  name     = "${local.name}-cart-node-pool"
+  location = local.zone
+  cluster  = google_container_cluster.cluster.name
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+
+    labels = {
+      app = "cart"
+    }
+
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["kubernetes-engine-node", "${local.project}-kubernetes-engine", "cart"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
+  management {
+    auto_repair = true
+  }
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "3"
   }
 
   depends_on = [
@@ -115,29 +133,38 @@ resource "google_container_node_pool" "cart_node_pool" {
 }
 
 resource "google_container_node_pool" "checkout_node_pool" {
-  project    = local.project
-  name       = "${local.name}-checkout-node-pool"
-  location   = local.zone
-  cluster    = google_container_cluster.cluster.name
-  node_count = "1"
+  project  = local.project
+  name     = "${local.name}-checkout-node-pool"
+  location = local.zone
+  cluster  = google_container_cluster.cluster.name
 
   node_config {
     oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/monitoring"
     ]
 
-  labels = {
-    env = local.project
-    app = "checkout"
-  }
+    labels = {
+      app = "checkout"
+    }
 
     preemptible  = true
     machine_type = "n1-standard-1"
-    tags         = [ "kubernetes-engine-node", "${local.project}-kubernetes-engine", "checkout" ]
+    tags         = ["kubernetes-engine-node", "${local.project}-kubernetes-engine", "checkout"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
+  }
+
+  management {
+    auto_repair = true
+  }
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "3"
   }
 
   depends_on = [
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "kubernetes_engine_cluster_name" {
   value       = google_container_cluster.cluster.name
   description = "Kubernetes Engine cluster name."
 }
+
+output "kubernetes_engine_cluster_endpoint" {
+  value       = google_container_cluster.cluster.endpoint
+  description = "Kubernetes Engine endpoint."
+}
